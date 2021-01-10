f551721e8cc3893ad8ccb73f395414a6e2104344
Haluk Dogan <haluk.dogan@huskers.unl.edu>
2021-01-10 12:14:19 -0600

    update dir-locals for python in a container

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,11 @@
-((nil . ((eval . (progn
-                   ;; set path to the python modules directory
-                   (add-to-list 'exec-path (concat (locate-dominating-file default-directory dir-locals-file) "bin/"))
-                   ;; configure inferior python shell.
-                   (setq python-shell-interpreter "pythondocker"))))))
+(
+ (python-mode . (
+                 (eval . (progn
+                           ;; set path to the python modules directory
+                           (add-to-list 'exec-path (concat (locate-dominating-file default-directory dir-locals-file) "bin/"))
+                           ;; configure inferior python shell.
+                           (setq-local python-shell-interpreter "pythondocker")
+                           (setq-local python-shell-interpreter-interactive-arg "-i")
+                           (setq-local python-shell-completion-native-enable nil)
+                           ))))
+ )
