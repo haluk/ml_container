6c640baf9ac4289c2d6306b521c5976fa41b4f5a
Haluk Dogan <haluk.dogan@huskers.unl.edu>
2021-01-10 01:27:14 -0600

    run python in docker container

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,5 @@
 ((nil . ((eval . (progn
-                   (add-to-list 'exec-path (concat (locate-dominating-file default-directory ".dir-locals.el") "bin/")))))))
+                   ;; set path to the python modules directory
+                   (add-to-list 'exec-path (concat (locate-dominating-file default-directory dir-locals-file) "bin/"))
+                   ;; configure inferior python shell.
+                   (setq python-shell-interpreter "pythondocker"))))))
